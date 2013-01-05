fa23c303f9a0d8d52b3d28003ef6fa7c095e46eb
Danilo Bargen <gezuru@gmail.com>
2013-01-05 04:47:17 +0100

    Lexer fixes

--- lexer.py
+++ lexer.py
@@ -1,42 +1,53 @@
 import ply.lex as lex
 
 tokens = [
-    'TYPE', 'ID', 'SPECIAL',
-    'INTEGER',
-    'STRING',
-    'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
-    'LPAREN', 'RPAREN', 'LCURLY', 'RCURLY',
-    'COLON', 'SEMICOLON', 'DOT',
+    'TYPE', 'ID',
+
+    'INTEGER', 'STRING', 'BOOL',
+
     'ASSIGN',
     'LESS', 'LESSEQUAL', 'EQUAL',
-    'INT_COMPLEMENT', 'BOOL_COMPLEMENT',
-    'COMMENT', 'KEYWORD',
-    'WHITESPACE']
+    'INT_COMPLEMENT', 'NOT',
 
-t_ignore = ' \n\f\r\t\v'  # matches t_WHITESPACE
+    'CLASS', 'INHERITS',
+    'IF', 'THEN', 'ELSE', 'FI',
+    'WHILE', 'LOOP', 'POOL',
+    'LET', 'IN',
+    'CASE', 'OF', 'ACTION', 'ESAC',
+    'NEW',
+    'ISVOID']
+
+literals = ['+', '-', '*', '/', ':', ';', '(', ')', '{', '}', '@', '.']
+
+t_ignore = ' \n\f\r\t\v'
 
 t_TYPE = r'[A-Z][A-Za-z0-9_]*'
 t_ID = r'[a-z][A-Za-z0-9_]*'
-t_SPECIAL = r'self|SELF_TYPE'
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_LCURLY = r'{'
-t_RCURLY = r'}'
-t_COLON = r':'
-t_SEMICOLON = r';'
-t_DOT = r'\.'
+
 t_ASSIGN = r'<-'
 t_LESS = r'<'
 t_LESSEQUAL = r'<='
 t_EQUAL = r'='
 t_INT_COMPLEMENT = r'~'
-t_BOOL_COMPLEMENT = r'[nN][oO][tT]'
-t_KEYWORD = r'class|else|false|fi|if|in|inherits|isvoid|let|loop|pool|then|while|case|esac|new|of|true'
-t_WHITESPACE = r'[ \n\f\r\t\v]+'
+t_NOT = r'[nN][oO][tT]'
+
+t_CLASS = r'class'
+t_INHERITS = r'inherits'
+t_IF = r'if'
+t_THEN = r'then'
+t_ELSE = r'else'
+t_FI = r'fi'
+t_WHILE = r'while'
+t_LOOP = r'loop'
+t_POOL = r'pool'
+t_LET = r'let'
+t_IN = r'in'
+t_CASE = r'case'
+t_OF = r'of'
+t_ACTION = r'=>'
+t_ESAC = r'esac'
+t_NEW = r'new'
+t_ISVOID = r'isvoid'
 
 def t_INTEGER(t):
     r'[0-9]+'
@@ -47,6 +58,10 @@ def t_STRING(t):
     r'"[^\0\n]*(\\\n[^\0\n]*)*"'
     t.value = t.value[1:-1]
     return t
+
+def t_BOOL(t):
+    r'true|false'
+    return True if t.value == 'true' else False
 
 def t_COMMENT(t):
     r'--[^\n]+\n|\(\*[^(\*\))]+\*\)'
